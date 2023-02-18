e930ad83c3d250f37741c05bde995fdd60981d3e
unknown <pedroppld@gmail.com>
2023-02-18 17:03:42 -0300

    corrigindo 'hidratation'

--- src/components/ToggleTheme.jsx/index.jsx
+++ src/components/ToggleTheme.jsx/index.jsx
@@ -6,17 +6,27 @@ import { Sun, Moon } from 'phosphor-react'
 
 
 export const ToggleTheme = () => {
-  const { theme, setTheme } = useTheme()  
+  const { theme, setTheme } = useTheme()
+  
 
+  const ChangeTheme = () => {
+    setTheme(!theme)
+  }
 
   return (
     <>
-
+      {
+        
+        theme === 'light'?
+          (
 
             <S.StyledButton onClick={() => setTheme('dark')}> <Moon color='#000' size={15} /></S.StyledButton>
-         
-            <S.StyledButton2 onClick={() => setTheme('light')}><Sun color='#fff' size={15} /></S.StyledButton2>
-      
+
+          ) :
+          (
+            <S.StyledButton onClick={() => setTheme('light')}><Sun color='#fff' size={15} /></S.StyledButton>
+          )
+      }
     </>
   )
 }
