10bf66eed6affa2c0ded528dc7ba75717fb7fc3e
unknown <pedroppld@gmail.com>
2023-03-04 19:38:18 -0300

    ajuste zoom map

--- src/components/LocalMap/index.jsx
+++ src/components/LocalMap/index.jsx
@@ -39,7 +39,7 @@ export const LocalMap = () => {
               <S.OpenMap onClick={handleMap}><XCircle size={32} /></S.OpenMap>
               <GoogleMap
 
-                zoom={18}
+                zoom={20}
                 center={mapCenter}
                 mapContainerStyle={{ width: '380px', height: '500px' }}
                 onLoad={() => console.log('Map Component Loaded...')}
--- src/components/LocalMap/style.jsx
+++ src/components/LocalMap/style.jsx
@@ -5,10 +5,10 @@ export const Container = styled.div`
 
 
 
-height: 100%;
+height: 120vh;
 width: 100%;
 position: absolute;
-top:263rem;
+top:260rem;
 left: 0;
 right: 0;
 bottom: 0;
@@ -22,18 +22,17 @@ background: linear-gradient(166deg, rgba(63,89,80,1) 53%, rgba(217,193,150,1) 10
 
 .marker{
   color: red;
-  margin-top: 35px;
+  margin-top: 50px;
 }
 
 `
 
 export const Container2 = styled.div`
-height: 75%;
+height: 60%;
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-gap: 2.2rem;
 margin: 7rem;
 
 `
@@ -46,6 +45,7 @@ font-size: .8rem;
 cursor: pointer;
 opacity: .7;
 margin: .2rem 0rem 1.5rem 0rem;
+
 @media (min-width: 500px){
   display: none;
 }
