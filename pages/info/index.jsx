7b201e1e79f264ac1b41c5182f36d692fbfb7ebc
unknown <pedroppld@gmail.com>
2023-03-14 18:12:12 -0300

    ajustes info page img

--- pages/info/index.jsx
+++ pages/info/index.jsx
@@ -10,7 +10,7 @@ export default function Info() {
     <>
       <Layout>
         <div>
-          <Image src="/images/consultorio.jpg" alt=""
+          <img src="/images/consultorio.jpg" alt=""
             height="250" width="415" />
         </div>
         <S.Container>
@@ -19,7 +19,7 @@ export default function Info() {
             <S.Text>Após a primeira reunião gratuita, você expressa seu problema e o que deseja em troca. E estamos chegando a esse resultado em cinco encontros. Cada reunião tem seu próprio propósito, portanto, após cada uma você obtém seus resultados Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod rerum temporibus labore earum, quam consequatur similique ad aut maxime rem voluptatibus eum beatae dicta sed perspiciatis ea, facere reprehenderit repellat!</S.Text>
           </S.ContainerText>
           <div>
-            <Image
+            <img
               src="/images/psicologa.webp"
               alt=""
               height="250" width="375"
