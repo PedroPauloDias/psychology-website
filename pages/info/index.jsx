40b62485963f3d05e6ffa4a09493e2b8205c4a73
unknown <pedroppld@gmail.com>
2023-03-14 19:24:10 -0300

    style change

--- pages/info/index.jsx
+++ pages/info/index.jsx
@@ -1,7 +1,6 @@
 import { FaqSimple } from "@/src/components/frequentlyQuestions";
 import Layout from "../Layout";
-
-
+import * as S from './style/style'
 
 export default function Info() {
   return (
@@ -11,11 +10,11 @@ export default function Info() {
           <img src='/images/consultorio.jpg' alt=''
             height="250" width="415" />
         </div>
-        <div>
-          <div>
-            <h2>Primeira Consulta</h2>
-            <p>Após a primeira reunião gratuita, você expressa seu problema e o que deseja em troca. E estamos chegando a esse resultado em cinco encontros. Cada reunião tem seu próprio propósito, portanto, após cada uma você obtém seus resultados Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod rerum temporibus labore earum, quam consequatur similique ad aut maxime rem voluptatibus eum beatae dicta sed perspiciatis ea, facere reprehenderit repellat!</p>
-          </div>
+        <S.Container>
+          <S.ContainerText>
+            <S.Title>Primeira Consulta</S.Title>
+            <S.Text>Após a primeira reunião gratuita, você expressa seu problema e o que deseja em troca. E estamos chegando a esse resultado em cinco encontros. Cada reunião tem seu próprio propósito, portanto, após cada uma você obtém seus resultados Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod rerum temporibus labore earum, quam consequatur similique ad aut maxime rem voluptatibus eum beatae dicta sed perspiciatis ea, facere reprehenderit repellat!</S.Text>
+          </S.ContainerText>
           <div>
             <img
               src='/images/psicologa.webp'
@@ -23,12 +22,12 @@ export default function Info() {
               height="250" width="375"
             />
           </div>
-          <div>
-            <h2>Nossas consultas</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestias minima nemo nihil molestiae consequatur esse quasi doloribus, exercitationem sint officia earum odio suscipit perspiciatis dolores corporis? Ab, atque eos Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium id nostrum molestias repellendus vero nulla ratione. Esse distinctio suscipit est! Officia praesentium laborum deserunt rem nulla? Assumenda ea atque obcaecati!.</p>
-          </div>
+          <S.ContainerText>
+            <S.Title>Nossas consultas</S.Title>
+            <S.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, molestias minima nemo nihil molestiae consequatur esse quasi doloribus, exercitationem sint officia earum odio suscipit perspiciatis dolores corporis? Ab, atque eos Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium id nostrum molestias repellendus vero nulla ratione. Esse distinctio suscipit est! Officia praesentium laborum deserunt rem nulla? Assumenda ea atque obcaecati!.</S.Text>
+          </S.ContainerText>
           <FaqSimple/>
-        </div>
+        </S.Container>
       </Layout>
     </>
   )
---
+++ pages/info/style/style.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+padding: 1rem 1.2rem;
+`
+
+export const ContainerText = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const Title = styled.h2`
+font-size: 2rem;
+margin-top: 2.5rem;
+`
+
+export const Text = styled.p`
+margin: 1.5rem 0;
+margin-bottom: 3rem;
+font-family: inter;
+font-size: 1.1rem;
+line-height: 1.5rem;
+`
+
