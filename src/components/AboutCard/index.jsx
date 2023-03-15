219f56d01ed1b1eda191236ed0999982ceb98c98
unknown <pedroppld@gmail.com>
2023-03-15 13:27:02 -0300

    aboutMe page

--- src/components/AboutCard/index.jsx
+++ src/components/AboutCard/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import {CaretRight} from 'phosphor-react'
+import { CaretRight } from 'phosphor-react'
+import { useRouter } from 'next/router'
+
 import * as S from './style'
 
-const AboutCardCard = ({title, description}) => {
+const AboutCardCard = ({ title, description , category}) => {
+  const { push } = useRouter()
+
   
   return (
     <S.Container >
       <S.Title data-aos="zoom-in-up">{title}</S.Title>
       <S.Description data-aos="zoom-in-up">{description}</S.Description>
-    <S.Button data-aos="zoom-in-up" href="#">Saiba mais <CaretRight/></S.Button>
+      <S.Button data-aos="zoom-in-up" href="#" onClick={() => push(`/about/${category}`)} >
+        Saiba mais <CaretRight /></S.Button>
   </S.Container>
   )
 }
--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -8,10 +8,12 @@ export const AboutSection = () => {
       <S.AboutContainer>
         <AboutCardCard
           title='Sobre mim'
+          category=""
           description='Sou psicóloga e psicoterapeuta particular. Pratico há 6 anos, pratico psicologia há 9 anos. Tenho um diploma de educação psicológica especial superior, um especialista certificado em PNL. Utilizo os métodos mais fortes desta escola, voltados a terapia para provocar mudanças'
         />
         <AboutCardCard
           title='Sobre o método'
+          category="method"
           description='Eu uso a escola psicológica da PNL em meu trabalho. Programação neurolinguística. O nome é muito assustador e assustador, mas não julgue pela capa. "Neuro" significa trabalhar com o sistema nervoso e as emoções. "Linguística" - trabalhar com palavras, linguagem '
         />
       </S.AboutContainer>
