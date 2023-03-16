3b89ba72e6b31cae5f825db7d2d9ca2ba3f83c8f
unknown <pedroppld@gmail.com>
2023-03-15 21:24:04 -0300

    update:  servico

--- src/components/ServicesContent/index.jsx
+++ src/components/ServicesContent/index.jsx
@@ -5,7 +5,7 @@ import { Form } from '@/src/components/Form'
 
 export async function getStaticProps() {
 
-  const res = await fetch('http://localhost:3000/api/services')
+  const res = await fetch('https://www.thaynacunha.com.br//api/services')
   const data = await res.json()
   console.log(data.services)
   return {
