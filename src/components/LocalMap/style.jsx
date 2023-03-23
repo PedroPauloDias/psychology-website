8b2e24edd88a6c5a9d07f897338e7f5d1be4669b
unknown <pedroppld@gmail.com>
2023-03-23 14:55:56 -0300

    ajuste map´

--- src/components/LocalMap/style.jsx
+++ src/components/LocalMap/style.jsx
@@ -5,9 +5,9 @@ export const Container = styled.div`
 
 position: absolute;
 
-height: 100vh;
+height: 100%;
 width: 100%;
-top:30rem;
+top:0rem;
 left: 0;
 right: 0;
 bottom: 0;
@@ -31,7 +31,7 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-margin-top: 10rem;
+margin-top: 30rem;
 
 `
 
